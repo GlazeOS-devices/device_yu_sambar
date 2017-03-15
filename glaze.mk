fba73850013f33b13da3f4b2f9feac3d1f223f44
NoelMacwan <yulespencer@gmail.com>
2017-03-15 13:44:32 +0530

    sambar: rebrand to GlazeOS

--- glaze.mk
+++ glaze.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/yu/sambar/full_sambar.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Glaze stuff.
+$(call inherit-product, vendor/glaze/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sambar
+PRODUCT_NAME := glaze_sambar
 BOARD_VENDOR := yu
 TARGET_VENDOR := yu
 PRODUCT_DEVICE := sambar
